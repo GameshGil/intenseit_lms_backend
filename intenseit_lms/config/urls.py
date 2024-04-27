293f47397d31ff9c39d5af870af5a90397e189c2
GameshGil <s.amirov90@mail.ru>
2024-04-27 11:22:35 +0300

    Update: добавлены url-ы для статей и токенов авторизации

--- intenseit_lms/config/urls.py
+++ intenseit_lms/config/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(('articles.urls', 'articles'),
+                            namespace='articles')),
+    path('token/', TokenObtainPairView.as_view(), name='user_token_obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(),
+         name='user_token_refresh'),
 ]
