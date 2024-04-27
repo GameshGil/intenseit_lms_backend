7fb1d69f3ad118933ede6ef4dddf48a7965b158e
GameshGil <s.amirov90@mail.ru>
2024-04-27 11:25:26 +0300

    Update: добавлен сериализатор для статей

---
+++ intenseit_lms/articles/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    """Сериализатор для статей."""
+
+    class Meta:
+        model = Article
+        fields = '__all__'
