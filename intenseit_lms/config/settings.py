14e0d2db05c983018569889e286f9c6b153bd65f
GameshGil <s.amirov90@mail.ru>
2024-04-27 11:23:32 +0300

    Update: добавлены настройки аутентификации и базовой пагинации

--- intenseit_lms/config/settings.py
+++ intenseit_lms/config/settings.py
@@ -117,6 +117,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
