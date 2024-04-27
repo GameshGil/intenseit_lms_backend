ea6e7e65bff9d4ba9eda38f3f76f3735ce5f7dec
GameshGil <s.amirov90@mail.ru>
2024-04-27 11:24:20 +0300

    Update: добавлена модель статей

--- intenseit_lms/articles/models.py
+++ intenseit_lms/articles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+
+class Article(models.Model):
+    """Модель статей."""
+
+    title = models.CharField('Заголовок статьи', max_length=255, blank=False)
+    text = models.TextField('Текст статьи', blank=False)
+    create_date = models.DateTimeField('Дата создания', auto_now_add=True)
+    update_date = models.DateTimeField('Дата изменения', auto_now=True)
+    pub_date = models.DateTimeField('Дата публикации', default=timezone.now)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='articles',
+        verbose_name='Автор публикации'
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self) -> str:
+        return self.title[:20]
