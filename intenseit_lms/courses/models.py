afd1268f4d821a13bec47a7ba84b8d303da8e20f
GameshGil <s.amirov90@mail.ru>
2024-05-04 15:34:00 +0300

    Fix: внесены правки в поля моделей area и course

--- intenseit_lms/courses/models.py
+++ intenseit_lms/courses/models.py
@@ -5,7 +5,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Area(models.Model):
     """Модель направлений курсов."""
 
-    name = models.CharField('Название направления', max_length=64)
+    name = models.CharField(
+        'Название направления',
+        max_length=64,
+        blank=False,
+        null=False
+    )
 
     class Meta:
         ordering = ['name']
@@ -29,6 +34,7 @@ class Course(models.Model):
         Area,
         verbose_name='Направление курса',
         on_delete=models.SET_NULL,
+        blank=True,
         null=True,
         related_name='courses'
     )
