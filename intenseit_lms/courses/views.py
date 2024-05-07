76bbd807875f7e95b853dfa1018982c26cf9d6ca
GameshGil <s.amirov90@mail.ru>
2024-05-07 15:00:51 +0300

    Feature: добавлены представления для направлений, курсов и аналитики

--- intenseit_lms/courses/views.py
+++ intenseit_lms/courses/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Area, Course
+from .serializers import CourseSerializer, AreaSerialiser
+
+
+class AreaViewSet(viewsets.ModelViewSet):
+    queryset = Area.objects.all()
+    serializer_class = AreaSerialiser
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+@api_view()
+def get_analytics(request):
+    return Response({'info': 'Набор аналитических данных.'})
