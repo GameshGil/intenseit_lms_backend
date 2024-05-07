d1b968549aa2799fbe39cae8edfaa6c92c553797
GameshGil <s.amirov90@mail.ru>
2024-05-08 01:10:26 +0300

    Refactor: внесены правки в именование пространств имен

--- intenseit_lms/config/urls.py
+++ intenseit_lms/config/urls.py
@@ -5,9 +5,14 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(('articles.urls', 'articles'),
-                            namespace='articles')),
-    path('api/v1/', include(('courses.urls', 'courses'), namespace='courses')),
+    path('api/v1/', include((
+        'articles.urls', 'articles'),
+        namespace='article_app'
+    )),
+    path('api/v1/', include((
+        'courses.urls', 'courses'),
+        namespace='course_app'
+    )),
     path('token/', TokenObtainPairView.as_view(), name='user_token_obtain'),
     path('token/refresh/', TokenRefreshView.as_view(),
          name='user_token_refresh'),
