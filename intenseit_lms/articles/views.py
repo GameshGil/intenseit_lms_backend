47245a2e9cabe117f7b3d64639b07caecb9178e8
GameshGil <s.amirov90@mail.ru>
2024-05-08 01:09:34 +0300

    Fix: небольшие правки на будущее

--- intenseit_lms/articles/views.py
+++ intenseit_lms/articles/views.py
@@ -5,5 +5,10 @@ from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
+    """Представление для статей."""
+
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+    # def perform_create(self, serializer):
+    #     serializer.save(author=self.request.user)
