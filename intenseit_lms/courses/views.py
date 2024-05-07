1805268b3696b054ee71bd92c668417117123850
GameshGil <s.amirov90@mail.ru>
2024-05-07 15:03:44 +0300

    Fix: удалено представление направлений

--- intenseit_lms/courses/views.py
+++ intenseit_lms/courses/views.py
@@ -2,13 +2,8 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Area, Course
-from .serializers import CourseSerializer, AreaSerialiser
-
-
-class AreaViewSet(viewsets.ModelViewSet):
-    queryset = Area.objects.all()
-    serializer_class = AreaSerialiser
+from .models import Course
+from .serializers import CourseSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
