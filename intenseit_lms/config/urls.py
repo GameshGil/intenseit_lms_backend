8f903c642a458031488a902ba45604848d9f5425
GameshGil <s.amirov90@mail.ru>
2024-05-07 15:06:44 +0300

    Feature: добавлен набор эндпоинтов для приложения courses

--- intenseit_lms/config/urls.py
+++ intenseit_lms/config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(('articles.urls', 'articles'),
                             namespace='articles')),
+    path('api/v1/', include(('courses.urls', 'courses'), namespace='courses')),
     path('token/', TokenObtainPairView.as_view(), name='user_token_obtain'),
     path('token/refresh/', TokenRefreshView.as_view(),
          name='user_token_refresh'),
