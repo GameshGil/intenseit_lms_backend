e88c85637cd33292098a05b51cdb1149e9aedae0
GameshGil <s.amirov90@mail.ru>
2024-05-26 14:37:05 +0300

    feature: изменен формат возвращаемых данных, добавлено поле url

--- intenseit_lms/tasks/serializers.py
+++ intenseit_lms/tasks/serializers.py
@@ -6,6 +6,14 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     """Сериализатор для заданий."""
 
+    def to_representation(self, instance):
+        data = {
+            'name': instance.name,
+            'text': instance.text,
+            'url': instance.get_absolute_url()
+        }
+        return data
+
     class Meta:
         model = Task
-        fields = ('name', 'text', 'lesson')
+        fields = ('name', 'text', 'article')
