ffee72bdc3f9590d262b6835e06aa3f94b570f50
GameshGil <s.amirov90@mail.ru>
2024-05-26 14:27:51 +0300

    refactor: url-ы ресурсов перенесены в один файл

--- intenseit_lms/articles/urls.py
+++ intenseit_lms/articles/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from . import views
+from .views import ArticleViewSet
+from courses.views import CourseViewSet, LessonViewSet, get_analytics
+from tasks.views import TaskViewSet
 
 router = DefaultRouter()
 
 
-router.register(r'articles', views.ArticleViewSet, basename='articles')
+router.register(r'articles', ArticleViewSet, basename='articles')
+router.register(r'courses', CourseViewSet, basename='courses')
+router.register(
+    r'courses/(?P<course_id>\d+)/lessons',
+    LessonViewSet,
+    basename='lessons'
+)
+router.register(r'tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('analytics/', get_analytics, name='analytics')
 ]
