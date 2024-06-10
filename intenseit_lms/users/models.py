163be4e81bc7abbffc8851d01a1322065513b0e5
GameshGil <s.amirov90@mail.ru>
2024-06-11 01:44:52 +0300

    update: добавлены канонический url-адрес, сортировка и подробное имя для модели CustomUser

--- intenseit_lms/users/models.py
+++ intenseit_lms/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from rest_framework.reverse import reverse
 
 
 class CustomUser(AbstractUser):
@@ -18,5 +19,13 @@ class CustomUser(AbstractUser):
         default=USER
     )
 
+    class Meta:
+        ordering = ['pk']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self) -> str:
         return self.username
+
+    def get_absolute_url(self):
+        return reverse('lms_api:users-detail', kwargs={'pk': self.pk})
