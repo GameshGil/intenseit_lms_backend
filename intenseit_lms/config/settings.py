fda8982511d527dbea2dd66f8b2937035852eafa
GameshGil <s.amirov90@mail.ru>
2024-06-11 01:48:16 +0300

    feature: добавлена аутентификация по JWT-токену и базовый класс разрешений

--- intenseit_lms/config/settings.py
+++ intenseit_lms/config/settings.py
@@ -127,6 +127,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
 
