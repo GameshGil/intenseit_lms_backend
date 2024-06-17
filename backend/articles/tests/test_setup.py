d77c114bc346700d77d36e66a57cb6e0124924ec
GameshGil <s.amirov90@mail.ru>
2024-06-17 15:29:27 +0300

    refactor: переделана структура абстрактного класса тестов

--- backend/articles/tests/test_setup.py
+++ backend/articles/tests/test_setup.py
@@ -12,23 +12,29 @@ User = get_user_model()
 
 class ArticleTestSetup(APITestCase):
 
-    def setUp(self):
-        self.user = User.objects.create(
+    @classmethod
+    def setUpTestData(cls) -> None:
+        User.objects.create(
             username='test_user1',
             password='Abcqwert1',
             role='u'
         )
+        area = Area.objects.create(name='Test area name')
+        Course.objects.create(
+            name='Test course name',
+            area=area,
+            year=2024,
+            is_hidden=False
+        )
+
+    def setUp(self) -> None:
+        self.user = User.objects.get(pk=1)
         refresh = RefreshToken.for_user(self.user)
         self.access_token = str(refresh.access_token)
         self.client.credentials(
             HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-        self.area = Area.objects.create(name='Test area name')
-        self.course = Course.objects.create(
-            name='Test course name',
-            area=self.area,
-            year=2024,
-            is_hidden=False
-        )
+        self.area = Area.objects.get(pk=1)
+        self.course = Course.objects.get(pk=1)
         self.article = Article.objects.create(
             title='Title of test article',
             text='Text of test article',
@@ -42,6 +48,6 @@ class ArticleTestSetup(APITestCase):
 
 class CourseArticleTestSetup(ArticleTestSetup):
 
-    def setUp(self):
+    def setUp(self) -> None:
         super().setUp()
         self.course_article = CourseArticle.objects.get(pk=1)
