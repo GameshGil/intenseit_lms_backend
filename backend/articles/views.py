3a7591cef1adb61bc952de723b788aa84b5221dc
GameshGil <s.amirov90@mail.ru>
2024-06-17 17:15:32 +0300

    update: добавлено сохранение автора при создании статьи

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -10,5 +10,5 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    # def perform_create(self, serializer):
-    #     serializer.save(author=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
